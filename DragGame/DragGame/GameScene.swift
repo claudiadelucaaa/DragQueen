c7e39814b4c5e7f9dd7df306b3f591b4a3648e67
feerdd <148528966+feerdd@users.noreply.github.com>
2023-12-06 12:20:55 +0100

    try

--- DragGame/DragGame/GameScene.swift
+++ DragGame/DragGame/GameScene.swift
@@ -83,6 +83,6 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // Called before each frame is rendered.
     }
 }
